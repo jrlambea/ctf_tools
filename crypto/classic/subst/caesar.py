c11bff7fa1d6d63b23291dd56e85aa9c00fe57a9
Jose Ramón Lambea <jrlambea@users.noreply.github.com>
2015-10-05 11:29:57 +0200

    Update caesar.py

--- crypto/classic/subst/caesar.py
+++ crypto/classic/subst/caesar.py
@@ -15,18 +15,34 @@ import argparse
 def main():
 
 	parser = argparse.ArgumentParser()
-	parser.add_argument ("text", help = "message to encrypt/decrypt", type = str)
-	parser.add_argument ("-d", "--displacement", help = "number of positions to displace", type = int, default = 3)
-	parser.add_argument ("-a", "--alphabet", help = "alphabet to use", type = str, default = 'abcdefghijklmnopqrstuvwxyz')
+	parser.add_argument ("text", help = "Message to encrypt/decrypt", type = str)
+	parser.add_argument ("-d", "--displacement", help = "Number of positions to displace", type = int, default = 3)
+	parser.add_argument ("-a", "--alphabet", help = "Alphabet to use", type = str, default = 'abcdefghijklmnopqrstuvwxyz')
+	parser.add_argument ("-b", "--brute-force", help = "Encode the given text for all the chars given in alphabet", action = "count", default = 0)
 	args = parser.parse_args()
 
 	alphabet = args.alphabet
 	d = args.displacement
 	text = args.text
 
+	if args.brute_force > 0:
+		for l in alphabet:
+			d = alphabet.index (l)
+			
+			for c in text:
+				if c in alphabet:
+					char = alphabet [(alphabet.index (c) + d) % len (alphabet)]
+					print ("%s" % char, end = "")
+				else:
+					print ("%s" % c, end = "")
+			print (" -- Using key: %c (%i)" % (l, d))
+
+		sys.exit (0)
+
+
 	for c in text:
 		if c in alphabet:
-			char = alphabet [(alphabet.index (c) + d) % len(alphabet)]
+			char = alphabet [(alphabet.index (c) + d) % len (alphabet)]
 			print ("%s" % char, end = "")
 		else:
 			print ("%s" % c, end = "")
